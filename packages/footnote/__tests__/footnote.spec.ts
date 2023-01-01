797c436cb47be5e5a0264b099c2229cf25344324
Mr.Hope <mister-hope@outlook.com>
2023-01-01 12:54:05 +0800

    test(footnote): add test for xhtml output

--- packages/footnote/__tests__/footnote.spec.ts
+++ packages/footnote/__tests__/footnote.spec.ts
@@ -4,10 +4,40 @@ import { footnote } from "../src/index.js";
 
 describe("footnote", () => {
   const markdownIt = MarkdownIt({ linkify: true }).use(footnote);
+  const markdownItXHTML = MarkdownIt({ linkify: true, xhtmlOut: true }).use(
+    footnote
+  );
 
   it("Pandoc example", () => {
     expect(
       markdownIt.render(
+        `
+Here is a footnote reference,[^1] and another.[^long-note]
+
+[^1]: Here is the footnote.
+
+[^long-note]: Here's one with multiple blocks.
+
+    Subsequent paragraphs are indented to show that they
+belong to the previous footnote.
+
+        { some.code }
+
+    The whole paragraph can be indented, or just the first
+    line.  In this way, multi-paragraph footnotes work like
+    multi-paragraph list items.
+
+This paragraph won’t be part of the note, because it
+isn’t indented.
+`,
+        {}
+      )
+    ).toMatchSnapshot();
+  });
+
+  it("Should support xhtml output", () => {
+    expect(
+      markdownItXHTML.render(
         `
 Here is a footnote reference,[^1] and another.[^long-note]
 
