1135f5f5d4b8c46081e27adbb47ad8d4dd8dd6fd
Mr.Hope <mister-hope@outlook.com>
2025-05-30 14:09:12 +0800

    feat(align): provide browser bundle

--- packages/align/rollup.config.ts
+++ packages/align/rollup.config.ts
@@ -1,5 +1,13 @@
 import { rollupTypescript } from "../../scripts/rollup.js";
 
-export default rollupTypescript("index", {
-  external: ["@mdit/plugin-container"],
-});
+export default [
+  ...rollupTypescript("index", {
+    external: ["@mdit/plugin-container"],
+  }),
+  ...rollupTypescript("index", {
+    output: {
+      file: "./lib/browser.js",
+    },
+    resolve: true,
+  }),
+];
