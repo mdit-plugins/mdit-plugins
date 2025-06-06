440c55841800fadba0e6b8db0f134d91efb745c7
Mister-Hope <mister-hope@outlook.com>
2025-06-06 13:30:56 +0800

    feat: improve

--- packages/attrs/__tests__/helper/getAttrs.spec.ts
+++ packages/attrs/__tests__/helper/getAttrs.spec.ts
@@ -1,6 +1,8 @@
+import { escapeHtml } from "@mdit/helper";
 import { describe, expect, it } from "vitest";
 
 import { getAttrs } from "../../src/helper/getAttrs.js";
+import { getDelimiterChecker } from "../../src/helper/getDelimiterChecker.js";
 import type { DelimiterConfig } from "../../src/helper/types.js";
 import type { MarkdownItAttrsOptions } from "../../src/index.js";
 import { replaceDelimiters } from "../replaceDelimiters.js";
@@ -42,6 +44,69 @@ const createDelimiterTests = (
         );
       },
     );
+
+    it(
+      replaceDelimiters(
+        'should parse attributes whose are ignored the key chars(\\t,\\n,\\f,\\s,/,>,",\',=) eg: {gt>=true slash/=trace i\\td "q\\fnu e\'r\\ny"=}',
+        options,
+      ),
+      () => {
+        const src = '{gt>=true slash/=trace i\td "q\fu\ne\'r\ny"=}';
+        const expected = [
+          ["gt", "true"],
+          ["slash", "trace"],
+          ["id", ""],
+          ["query", ""],
+        ];
+
+        expect(getAttrs(replaceDelimiters(src, options), 0, options)).toEqual(
+          expected,
+        );
+      },
+    );
+
+    it(
+      replaceDelimiters(
+        "should throw an error while calling `hasDelimiters` with an invalid `where` param",
+        options,
+      ),
+      () => {
+        // @ts-expect-error: error in test
+        expect(() => getDelimiterChecker(options, 0)).toThrow(
+          /Invalid 'where' parameter/,
+        );
+        // @ts-expect-error: error in test
+        expect(() => getDelimiterChecker(options, "")).toThrow(
+          /Invalid 'where' parameter/,
+        );
+        // @ts-expect-error: error in test
+        expect(() => getDelimiterChecker(options, null)).toThrow(
+          /Invalid 'where' parameter/,
+        );
+        // @ts-expect-error: error in test
+        expect(() => getDelimiterChecker(options, undefined)).toThrow(
+          /Invalid 'where' parameter/,
+        );
+        expect(() =>
+          // @ts-expect-error: error in test
+          getDelimiterChecker(options, "center")("has {#test} delimiters"),
+        ).toThrow(/Invalid 'where' parameter/);
+      },
+    );
+
+    it('should escape html entities(&,<,>,") eg: <a href="?a&b">TOC</a>', () => {
+      const src = '<a href="a&b">TOC</a>';
+      const expected = "&lt;a href=&quot;a&amp;b&quot;&gt;TOC&lt;/a&gt;";
+
+      expect(escapeHtml(src)).toEqual(expected);
+    });
+
+    it('should keep the origional input which is not contains(&,<,>,") char(s) eg: |a|b|', () => {
+      const src = "|a|b|";
+      const expected = "|a|b|";
+
+      expect(escapeHtml(src)).toEqual(expected);
+    });
   });
 };
 
--- packages/attrs/src/helper/getDelimiterChecker.ts
+++ packages/attrs/src/helper/getDelimiterChecker.ts
@@ -3,12 +3,17 @@ import type { DelimiterConfig } from "./types.js";
 
 export type DelimiterChecker = (content: string) => boolean;
 
-export const getDelimiterChecker =
-  (
-    { left, right }: Required<DelimiterConfig>,
-    where: "start" | "end" | "only",
-  ): DelimiterChecker =>
-  (content: string): boolean => {
+export const getDelimiterChecker = (
+  { left, right }: Required<DelimiterConfig>,
+  where: "start" | "end" | "only",
+): DelimiterChecker => {
+  if (!["start", "end", "only"].includes(where)) {
+    throw new Error(
+      `Invalid 'where' parameter: ${where}. Expected 'start', 'end', or 'only'.`,
+    );
+  }
+
+  return (content: string): boolean => {
     const leftLength = left.length;
     const rightLength = right.length;
     // we need minimum three chars, for example {b}
@@ -33,47 +38,29 @@ export const getDelimiterChecker =
     let slice: string;
     let nextChar: string;
 
-    switch (where) {
-      case "start": {
-        // first char should be {, } found in char 2 or more
-        slice = content.slice(0, leftLength);
-        start = slice === left ? 0 : -1;
-        end =
-          start === -1
-            ? -1
-            : content.indexOf(right, rightDelimiterMinimumShift);
-        // check if next character is not one of the delimiters
-        nextChar = content.charAt(end + rightLength);
-        if (nextChar && right.includes(nextChar)) end = -1;
-        break;
-      }
-
-      case "end": {
-        // last char should be }
-        start = content.lastIndexOf(left);
-        end =
-          start === -1
-            ? -1
-            : content.indexOf(right, start + rightDelimiterMinimumShift);
-        end = end === content.length - rightLength ? end : -1;
-        break;
-      }
-
-      case "only": {
-        // '{.a}'
-        slice = content.slice(0, leftLength);
-        start = slice === left ? 0 : -1;
-        slice = content.slice(content.length - rightLength);
-        end = slice === right ? content.length - rightLength : -1;
-        break;
-      }
-
-      default: {
-        throw new Error(
-          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-          `Invalid 'where' parameter: ${where}. Expected 'start', 'end', or 'only'.`,
-        );
-      }
+    if (where === "start") {
+      // first char should be {, } found in char 2 or more
+      slice = content.slice(0, leftLength);
+      start = slice === left ? 0 : -1;
+      end =
+        start === -1 ? -1 : content.indexOf(right, rightDelimiterMinimumShift);
+      // check if next character is not one of the delimiters
+      nextChar = content.charAt(end + rightLength);
+      if (nextChar && right.includes(nextChar)) end = -1;
+    } else if (where === "end") {
+      // last char should be }
+      start = content.lastIndexOf(left);
+      end =
+        start === -1
+          ? -1
+          : content.indexOf(right, start + rightDelimiterMinimumShift);
+      end = end === content.length - rightLength ? end : -1;
+    } else {
+      // '{.a}'
+      slice = content.slice(0, leftLength);
+      start = slice === left ? 0 : -1;
+      slice = content.slice(content.length - rightLength);
+      end = slice === right ? content.length - rightLength : -1;
     }
 
     return (
@@ -82,3 +69,4 @@ export const getDelimiterChecker =
       validCurlyLength(content.substring(start, end + rightLength))
     );
   };
+};
