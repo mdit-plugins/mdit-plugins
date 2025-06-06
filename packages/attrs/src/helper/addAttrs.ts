7ddb084a814a054428104b4399ef5801e257a2ff
Mister-Hope <mister-hope@outlook.com>
2025-06-06 11:33:45 +0800

    chore: tweaks

--- packages/attrs/src/helper/addAttrs.ts
+++ packages/attrs/src/helper/addAttrs.ts
@@ -3,12 +3,18 @@ import type Token from "markdown-it/lib/token.mjs";
 import type { Attr } from "./types.js";
 
 export const addAttrs = (attrs: Attr[], token: Token | null): void => {
-  if (token)
-    attrs.forEach((attrItem) => {
-      const [key, value] = attrItem;
+  if (!token) return;
 
-      if (key === "class") token.attrJoin("class", value);
-      else if (key === "css-module") token.attrJoin("css-module", value);
-      else token.attrPush(attrItem);
-    });
+  attrs.forEach(([key, value]) => {
+    switch (key) {
+      case "class":
+        token.attrJoin("class", value);
+        break;
+      case "css-module":
+        token.attrJoin("css-module", value);
+        break;
+      default:
+        token.attrPush([key, value]);
+    }
+  });
 };
