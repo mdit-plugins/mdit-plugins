8f844628fee49c3dde8643a70f221a34b485075d
Mister-Hope <mister-hope@outlook.com>
2026-02-09 15:49:05 +0800

    test(img-mark): improve coverage

--- packages/img-mark/__tests__/imgMark.spec.ts
+++ packages/img-mark/__tests__/imgMark.spec.ts
@@ -1,5 +1,5 @@
 import MarkdownIt from "markdown-it";
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { imgMark } from "../src/index.js";
 
@@ -9,45 +9,51 @@ const markdownItWithCustomOptions = MarkdownIt({ linkify: true }).use(imgMark, {
   dark: ["darkmode"],
 });
 
-it("should render", () => {
-  expect(markdownIt.render(`![image](/logo.svg)`)).toEqual(
-    '<p><img src="/logo.svg" alt="image"></p>\n',
-  );
+describe(imgMark, () => {
+  it("should render", () => {
+    expect(markdownIt.render(`![image](/logo.svg)`)).toEqual(
+      '<p><img src="/logo.svg" alt="image"></p>\n',
+    );
 
-  expect(markdownIt.render(`![image](/logo.svg#light)`)).toEqual(
-    '<p><img src="/logo.svg" alt="image" data-mode="lightmode-only"></p>\n',
-  );
-  expect(markdownIt.render(`![image](/logo.svg#dark)`)).toEqual(
-    '<p><img src="/logo.svg" alt="image" data-mode="darkmode-only"></p>\n',
-  );
-});
+    expect(markdownIt.render(`![image](/logo.svg#light)`)).toEqual(
+      '<p><img src="/logo.svg" alt="image" data-mode="lightmode-only"></p>\n',
+    );
+    expect(markdownIt.render(`![image](/logo.svg#dark)`)).toEqual(
+      '<p><img src="/logo.svg" alt="image" data-mode="darkmode-only"></p>\n',
+    );
+  });
 
-it("should support options", () => {
-  expect(markdownItWithCustomOptions.render(`![image](/logo.svg#lightmode)`)).toEqual(
-    '<p><img src="/logo.svg" alt="image" data-mode="lightmode-only"></p>\n',
-  );
+  it("should support options", () => {
+    expect(markdownItWithCustomOptions.render(`![image](/logo.svg#lightmode)`)).toEqual(
+      '<p><img src="/logo.svg" alt="image" data-mode="lightmode-only"></p>\n',
+    );
 
-  expect(markdownItWithCustomOptions.render(`![image](/logo.svg#darkmode)`)).toEqual(
-    '<p><img src="/logo.svg" alt="image" data-mode="darkmode-only"></p>\n',
-  );
-});
+    expect(markdownItWithCustomOptions.render(`![image](/logo.svg#darkmode)`)).toEqual(
+      '<p><img src="/logo.svg" alt="image" data-mode="darkmode-only"></p>\n',
+    );
+  });
 
-it("should not be effected by title", () => {
-  expect(markdownIt.render(`![image](/logo.svg#light "title")`)).toEqual(
-    '<p><img src="/logo.svg" alt="image" title="title" data-mode="lightmode-only"></p>\n',
-  );
+  it("should not be effected by title", () => {
+    expect(markdownIt.render(`![image](/logo.svg#light "title")`)).toEqual(
+      '<p><img src="/logo.svg" alt="image" title="title" data-mode="lightmode-only"></p>\n',
+    );
 
-  expect(markdownIt.render(`![image](/logo.svg#dark "another title")`)).toEqual(
-    '<p><img src="/logo.svg" alt="image" title="another title" data-mode="darkmode-only"></p>\n',
-  );
+    expect(markdownIt.render(`![image](/logo.svg#dark "another title")`)).toEqual(
+      '<p><img src="/logo.svg" alt="image" title="another title" data-mode="darkmode-only"></p>\n',
+    );
 
-  expect(markdownItWithCustomOptions.render(`![image](/logo.svg#lightmode "title")`)).toEqual(
-    '<p><img src="/logo.svg" alt="image" title="title" data-mode="lightmode-only"></p>\n',
-  );
+    expect(markdownItWithCustomOptions.render(`![image](/logo.svg#lightmode "title")`)).toEqual(
+      '<p><img src="/logo.svg" alt="image" title="title" data-mode="lightmode-only"></p>\n',
+    );
 
-  expect(
-    markdownItWithCustomOptions.render(`![image](/logo.svg#darkmode "another title")`),
-  ).toEqual(
-    '<p><img src="/logo.svg" alt="image" title="another title" data-mode="darkmode-only"></p>\n',
-  );
+    expect(
+      markdownItWithCustomOptions.render(`![image](/logo.svg#darkmode "another title")`),
+    ).toEqual(
+      '<p><img src="/logo.svg" alt="image" title="another title" data-mode="darkmode-only"></p>\n',
+    );
+  });
+
+  it("should handle empty src", () => {
+    expect(markdownIt.render("![]()")).toEqual('<p><img src="" alt=""></p>\n');
+  });
 });
